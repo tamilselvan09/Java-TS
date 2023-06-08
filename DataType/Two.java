36ad2002d8ca3b31a749b8c3a034589c01ae910d
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-08 12:41:51 +0530

    reference

--- DataType/Two.java
+++ DataType/Two.java
@@ -6,9 +6,11 @@ public class Two {
     public static void main(String[] args) {
         String ename = "Tamil Selvan";
         int[] ids = {101,102,103,104};
+         String[] size ={"S","M","L"};
 
 
         System.out.println(ename); //Tamil Selvan
         System.out.println(ids);   // their is no value of this one because its int so I@1188e820
+        System.out.println(size);
     }
 }

9fba34f410bbd219ab54e6a4b71fcb6d631c2677
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-09 13:40:28 +0530

    Data

---
+++ Variables/Test.java
@@ -0,0 +1,18 @@
+package Variables;
+
+public class Test {
+    int a = 10;
+    static int b = 20;
+
+    public static void main(String[] args) {
+        int c = 30;
+        Test obj1 = new Test();
+        //Test obj2 = new Test();
+
+
+        System.out.println(obj1.a);
+        System.out.println(b);
+        System.out.println(c);      //30
+    }
+
+}

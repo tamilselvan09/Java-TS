4286b2fbed66938942342e2568aaea6757a518c3
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-12 12:44:55 +0530

    CLP

---
+++ CommentLine/One.java
@@ -0,0 +1,11 @@
+package CommentLine;
+  // we write 10,20 in comment line panal 
+  // they are string so its not addition its concatination
+public class One {                                      
+    public static void main(String[] args) {
+        System.out.println(args.length);   //0
+
+        System.out.println(args[0] + args[1]);    //1020
+    }
+    
+}

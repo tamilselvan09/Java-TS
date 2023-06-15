b917372cc8b8f03b5787dd25f4be231c7bca0488
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-15 23:26:57 +0530

    okk

--- Operators/Test.java
+++ Operators/Test.java
@@ -1,4 +1,4 @@
-package Operators;
+
 
 public class Test {
     public static void main(String[] args) {
--- Operators/Test1.java
+++ Operators/Test1.java
@@ -1,4 +1,4 @@
-package Operators;
+
 
 public class Test1 {
     public static void main(String[] args) {

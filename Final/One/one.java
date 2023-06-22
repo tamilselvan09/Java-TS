c7eb8073e0010944661c4f785407ef26c3f1506c
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-22 22:38:07 +0530

    final

---
+++ Final/One/one.java
@@ -0,0 +1,12 @@
+package Final.One;
+
+class A {
+    public final void m1(){
+
+    }
+}
+class B extends A{
+    public void m2(){
+        
+    }
+}
---
+++ Final/one.java
@@ -0,0 +1,8 @@
+// package Final;
+
+// final class A{
+
+// };
+// class B extends A {
+
+// };  //We cant extents class A because its final

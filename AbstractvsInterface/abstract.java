80084ee963121f72300b24d23dd3e4fbccd922d9
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-22 22:45:03 +0530

    g

---
+++ AbstractvsInterface/abstract.java
@@ -0,0 +1,13 @@
+package AbstractvsInterface;
+abstract class Dao{
+    static int status = 100;
+}
+class DaoChild extends Dao{
+
+}
+class AbstractVar{
+    public static void main(String[] args) {
+        System.out.println(DaoChild.status);
+        new DaoChild();
+    }
+}

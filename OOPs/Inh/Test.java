aadb3c137eef3e06d692df566006819f21f26dab
tamilselvan09 <tamilselvan4982@gmail.com>
2023-06-27 23:16:29 +0530

    inh

---
+++ OOPs/Inh/Test.java
@@ -0,0 +1,23 @@
+class parent {
+      
+      public void m1(){}
+      public void m2(){}
+      
+}
+
+class child extends parent{
+
+       public void m3(){}
+}
+
+public class Test{
+       public static void main(String[] args){
+        parent p1=new parent();
+        p1.m1();
+        p1.m2();
+        //p1.m3();  //method not found
+      } 
+}
+
+
+//due to inheritance parent has only 2 method , child has 3 method 

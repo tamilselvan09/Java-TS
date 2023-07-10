e8d9fa34b5617d567393652be9f465e53f87f4e3
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-10 22:21:02 +0530

    try

---
+++ EH1/Test.java
@@ -0,0 +1,16 @@
+
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            Thread.sleep(5000);
+        } 
+        catch (Exception e) {
+          e.printStackTrace();
+            
+        }
+    }
+}
+
+
+     //Handling the exception using try catch
---
+++ EH1/TestOne.java
@@ -0,0 +1,10 @@
+//InterruptedException
+
+class TestOne{
+    public static void main(String[] args){
+        Thread.sleep(5000);
+    }
+}
+
+//We can't excicute line number 5 because it creacing the exception 
+//we excicute line number 5 using try catch or throws

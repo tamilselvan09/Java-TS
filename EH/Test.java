204c94f573f6b0dde71bf4e4921a8436f1cf3651
tamilselvan09 <tamilselvan4982@gmail.com>
2023-07-10 22:07:31 +0530

    AE

---
+++ EH/Test.java
@@ -0,0 +1,9 @@
+package EH;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(10/0);
+    }
+}
+
+//Code gendrate Exception 
---
+++ EH/TestOne.java
@@ -0,0 +1,10 @@
+package EH;
+
+public class TestOne {
+   public static void main(String[] args) {
+       throw new ArithmeticException("/ by Zero");
+   }   
+}
+
+
+//We are creation the Exception
